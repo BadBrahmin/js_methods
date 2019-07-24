8b13ef71202cb716ec93768f8c515a60c5f06df6
BadBrahmin <tejasshree95@gmail.com>
2019-07-24 21:43:56 +0530

    add 7-Array

--- array-methods/3-array.js
+++ array-methods/3-array.js
@@ -35,7 +35,10 @@ var words2 = [
 
 // Write a function doesWordExist that will take in an array of words as one argument, and a word to search for as the other. Return true if it exists, otherwise, return false. Don't use indexOf for this one.
 
+let doesWordExist = (arr, element) => arr.includes(element);
 
+console.log(doesWordExist(words2, 'sir')); 
+console.log(doesWordExist(words2, 'subset')); 
 
 
 var words3 = [
